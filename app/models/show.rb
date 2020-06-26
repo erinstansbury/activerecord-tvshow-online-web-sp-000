b03d0479c992acc0481c441adc0d752eb3ff2d84
Erin Stansbury <ekstansbury@gmail.com>
2020-06-26 01:32:16 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,8 @@ class Show < ActiveRecord::Base
   def Show::highest_rating
     Show.maximum(:rating)
   end
+  
+  def Show::most_popular_show
+    Show.where("rating == ?", Show.highest_rating).first
+  end
 end
